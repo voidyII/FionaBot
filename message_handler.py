0da1e8825c32e0d7a43afa7f686c710d7a678ffd
voidyII <christopher.vitz@outlook.de>
2023-11-15 20:21:05 +0100

    started working on role assigning

--- message_handler.py
+++ message_handler.py
@@ -1,8 +1,28 @@
-# import discord
-import os
+#import discord
 import response_handler
+import json
 
 # sends response
 async def send_msg(message, user_message):
-    response = response_handler.handle_all(user_message)
-    await message.channel.send(response)
+    user_message = user_message.lower()
+    if user_message != "rolemsg":
+        response = response_handler.handle_all(user_message)
+        await message.channel.send(response)
+    else:
+        moji = await  message.channel.send("test")
+        await moji.add_reaction('💻')
+
+        dic = {
+            "id": moji.channel.id,
+            "name": moji.channel.name
+        }
+
+        print("dic created")
+
+        id_file = open(".\id_file.json", "w")
+
+        if id_file
+        json.dump(dic, id_file)
+        id_file.close()
+
+        print("write/append succesful")
