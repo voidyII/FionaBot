b2985a799853488dbfbb72eb8c50135d9a9aa420
voidyII <christopher.vitz@outlook.de>
2023-11-17 01:43:57 +0100

    i have to rework the json stuff completely

--- message_handler.py
+++ message_handler.py
@@ -12,17 +12,27 @@ async def send_msg(message, user_message):
         moji = await  message.channel.send("test")
         await moji.add_reaction('💻')
 
-        dic = {
+        to_add = {
             "id": moji.channel.id,
-            "name": moji.channel.name
+            "name": moji.channel.name,
+            "type": moji.channel.type
         }
 
-        print("dic created")
+        # print("dic created")
 
-        id_file = open(".\id_file.json", "w")
+        # # jsfile = open(".\id_file.json", "w")
+        # id_file = json.load(open(".\id_file.json", "r"))
 
-        if id_file
-        json.dump(dic, id_file)
-        id_file.close()
+        # new_id_file = id_file.append(to_add)
+        
+        # json.dump(new_id_file, open(".\id_file.json", "w"))
+        # id_file.close()
+
+        #I FOUND THIS ON STACKOVERFLOW, THIS IS NOT THE SOLUTION I WANT BUT IT WORKS SOMEHOW
+        with open(".\id_file.json", mode="r+") as outfile:
+            outfile.seek(0,2)
+            pos = outfile.tell()-1
+            outfile.seek(pos)
+            outfile.write(", {}]".format(json.dumps(to_add)))
 
         print("write/append succesful")
